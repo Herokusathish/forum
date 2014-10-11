ab3dbf96968c5c15b09aa073096f00d28e5c5764
Sathish Heroku <sathish.heroku@gmail.com>
2014-10-11 04:39:48 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Forum::Application.config.secret_key_base = '8e456fa68f748b97d2a0337a3ab3718b9b659ee044b182a466ddad014b4dfb5842c91da097738deded292170ac56478c639ab1b68e7db2522c44d60c35625d63'
